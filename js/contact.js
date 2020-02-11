84543f42bee5ece34f635ba5da5c1f3c0b77b680
Bauter <bauter09@gmail.com>
2020-02-10 18:11:41 -0600

    changing post method to get in contact.js, index.html, and contact.php

--- js/contact.js
+++ js/contact.js
@@ -14,7 +14,7 @@ $(function() {
 		var formData = $(form).serialize();
 		// Submit the form using AJAX.
 		$.ajax({
-			type: 'POST',
+			type: 'GET',
 			url: $(form).attr('action'),
 			data: formData
 		})
